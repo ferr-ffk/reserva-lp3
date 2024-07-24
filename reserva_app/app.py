055d2deb458e22178691ad9505c4a04a5ac0db2b
Fernando Freitas <freitax.li@gmail.com>
2024-07-24 17:51:12 -0300

    adicionar comentários para descrição

--- reserva_app/app.py
+++ reserva_app/app.py
@@ -2,8 +2,10 @@ from flask import render_template, app, Flask
 
 app = Flask(__name__)
 
+# As reservas serão feitas e armazenadas na lista de reservas     
 reservas = []
 
+# As salas serão cadastradas e armazenadas na lista de salas
 lista_salas = []
 
 
@@ -25,6 +27,7 @@ def pagina_reservas() -> None:
 @app.route("/reservar-sala")
 def pagina_reservar_sala() -> None:
     return render_template("reservar-sala.html")
+
 
 @app.route("/detalhe-reserva")
 def pagina_detalhe_reserva() -> None:
@@ -49,5 +52,8 @@ def teste():
 
 
 if __name__ == "__main__":
+    # Define que a aplicação rodará no modo debug, para que não seja necessário reinicar o servidor toda vez que uma mudança for feita
     app.debug = True
+    
+    # Para iniciar o servidor, é necessário apenas executar o código, ao invés de rodar a aplicação pela linha de comando
     app.run()
