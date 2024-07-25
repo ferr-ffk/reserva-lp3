10aaba1dbe10939d7b8e638455b5b927c806e447
Fernando Freitas <freitax.li@gmail.com>
2024-07-25 17:42:10 -0300

    adicionar constante para melhor eficiência de código

--- reserva_app/reserva_service.py
+++ reserva_app/reserva_service.py
@@ -1,15 +1,21 @@
-from dicionario_csv import obter_lista_dicionarios_em_csv
+from dicionario_csv import obter_lista_dicionarios_em_csv, obter_dicionario_por_numero_linha_csv
 
 # Objeto padrão
 
 # -------------------------------------------------------
+
+ARQUIVO_LISTA_RESERVAS = "lista_reservas.csv"
 
 def criar_reservas(reserva: dict) -> None:
     pass
 
 
 def obter_reservas() -> list[dict]:
-    return obter_lista_dicionarios_em_csv("lista_reservas.csv")
+    return obter_lista_dicionarios_em_csv(ARQUIVO_LISTA_RESERVAS)
+
+
+def obter_reserva(id: int) -> dict:
+    return obter_dicionario_por_numero_linha_csv(id, ARQUIVO_LISTA_RESERVAS)
 
 
 def atualizar_reserva(id: int, nova_reserva: dict) -> None:
--- reserva_app/sala_service.py
+++ reserva_app/sala_service.py
@@ -1,15 +1,21 @@
-from dicionario_csv import obter_lista_dicionarios_em_csv
+from dicionario_csv import obter_lista_dicionarios_em_csv, obter_dicionario_por_numero_linha_csv
 
 # Objeto padrão
 
 # -------------------------------------------------------
+
+ARQUIVO_LISTA_SALAS = "lista_salas.csv"
 
 def criar_salas(sala: dict) -> None:
     pass
 
 
 def obter_salas() -> list[dict]:
-    return obter_lista_dicionarios_em_csv("lista_salas.csv")
+    return obter_lista_dicionarios_em_csv(ARQUIVO_LISTA_SALAS)
+
+
+def obter_sala(id: int) -> dict:
+    return obter_dicionario_por_numero_linha_csv(id, ARQUIVO_LISTA_SALAS)
 
 
 def atualizar_sala(id: int, nova_sala: dict) -> None:
