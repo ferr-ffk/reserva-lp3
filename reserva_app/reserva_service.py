d8cfbef9b31af4bbc19e708edcaf0732ee0c3b68
Fernando Freitas <freitax.li@gmail.com>
2024-07-29 17:29:42 -0300

    atualizar função

--- reserva_app/reserva_service.py
+++ reserva_app/reserva_service.py
@@ -32,11 +32,8 @@ def criar_reserva(codigo, usuario, sala, data_hora_inicio, data_hora_fim) -> Non
     reservas = obter_reservas()
 
     reserva_dic = reserva(codigo, usuario, sala, data_hora_inicio, data_hora_fim)
-
-    # Verifica se uma reserva com esse código já foi criada
-    codigo_ja_existe = reserva_dic['codigo'] in [key for key in reservas]
     
-    if codigo_ja_existe:
+    if codigo_existe(codigo):
         raise ValueError("Uma reserva com esse código já existe!")
 
     Dicionario_Csv.salvar_dicionario_em_arquivo(reserva_dic, ARQUIVO_LISTA_RESERVAS)
@@ -58,7 +55,7 @@ def deletar_reserva(id: int) -> None:
     Dicionario_Csv.excluir_linha_arquivo_por_padrao(f'id:{id}', ARQUIVO_LISTA_RESERVAS)
 
 
-def checar_codigo_em_reservas(codigo: str) -> bool:
+def codigo_existe(codigo: str) -> bool:
     """Checa se um código existe na lista de reservas
 
     Args:
@@ -70,14 +67,8 @@ def checar_codigo_em_reservas(codigo: str) -> bool:
 
     reservas = obter_reservas()
 
-    print(reservas)
-
     for reserva in reservas:
         if codigo == reserva['codigo']:
             return True
 
     return False
-
-# criar_reserva(90, "Latorre", "D4", "08:30", "10:00")
-
-checar_codigo_em_reservas(90)
