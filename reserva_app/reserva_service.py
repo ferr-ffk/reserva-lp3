f9ace13e57c09867b3eda22df572e74fc2ffac2d
Fernando Freitas <freitax.li@gmail.com>
2024-07-29 17:16:42 -0300

    adicionar comentário para função

--- reserva_app/reserva_service.py
+++ reserva_app/reserva_service.py
@@ -15,18 +15,33 @@ def reserva(codigo, usuario, sala, data_hora_inicio, data_hora_fim):
 
 ARQUIVO_LISTA_RESERVAS = "lista_reservas.csv"
 
-def criar_reserva(reserva: dict) -> None:
+def criar_reserva(codigo, usuario, sala, data_hora_inicio, data_hora_fim) -> None:
+    """Cria uma reserva e armazena no arquivo .csv
+
+    Args:
+        codigo (int): O código da sala em um número inteiro
+        usuario (str): O nome do usuário responsável pela reserva
+        sala (int): O código da sala
+        data_hora_inicio (str): O horário de início da reserva
+        data_hora_fim (str): O horário de término da reserva
+
+    Raises:
+        ValueError: Se uma reserva já possuir esse código
+    """
+
     reservas = obter_reservas()
+
+    reserva_dic = reserva_dic(codigo, usuario, sala, data_hora_inicio, data_hora_fim)
 
     chaves_dicionario = [key for key in reservas]
 
     # Verifica se uma reserva com esse código já foi criada
-    codigo_ja_existe = reserva['codigo'] in chaves_dicionario
+    codigo_ja_existe = reserva_dic['codigo'] in chaves_dicionario
     
     if codigo_ja_existe:
         raise ValueError("Uma reserva com esse código já existe!")
 
-    Dicionario_Csv.salvar_dicionario_em_arquivo(reserva, ARQUIVO_LISTA_RESERVAS)
+    Dicionario_Csv.salvar_dicionario_em_arquivo(reserva_dic, ARQUIVO_LISTA_RESERVAS)
 
 
 def obter_reservas() -> list[dict]:
