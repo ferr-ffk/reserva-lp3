7539d95478cbaecaf0cdec998d4dabe386ea02b4
Fernando Freitas <freitax.li@gmail.com>
2024-07-29 17:04:27 -0300

    criar função de criar reserva

--- reserva_app/reserva_service.py
+++ reserva_app/reserva_service.py
@@ -2,8 +2,7 @@ from dicionario_csv import Dicionario_Csv
 
 # Objeto padrão
 def reserva(codigo, usuario, sala, data_hora_inicio, data_hora_fim):
-    reserva = dict()
-    {
+    return {
         "codigo": codigo,
         "usuario": usuario,
         "sala": sala,
@@ -11,12 +10,23 @@ def reserva(codigo, usuario, sala, data_hora_inicio, data_hora_fim):
         "data e hora do fim": data_hora_fim,
         "ativa": True
     }
+
 # -------------------------------------------------------
 
 ARQUIVO_LISTA_RESERVAS = "lista_reservas.csv"
 
-def criar_reservas(reserva: dict) -> None:
-    pass
+def criar_reserva(reserva: dict) -> None:
+    reservas = obter_reservas()
+
+    chaves_dicionario = [key for key in reservas]
+
+    # Verifica se uma reserva com esse código já foi criada
+    codigo_ja_existe = reserva['codigo'] in chaves_dicionario
+    
+    if codigo_ja_existe:
+        raise ValueError("Uma reserva com esse código já existe!")
+
+    Dicionario_Csv.salvar_dicionario_em_arquivo(reserva, ARQUIVO_LISTA_RESERVAS)
 
 
 def obter_reservas() -> list[dict]:
