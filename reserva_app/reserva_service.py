4ce0593e98c83981ad04c91407eef8ec2dc30fee
Fernando Freitas <freitax.li@gmail.com>
2024-07-29 17:27:04 -0300

    criar função para checar código existente

--- reserva_app/reserva_service.py
+++ reserva_app/reserva_service.py
@@ -6,8 +6,8 @@ def reserva(codigo, usuario, sala, data_hora_inicio, data_hora_fim):
         "codigo": codigo,
         "usuario": usuario,
         "sala": sala,
-        "data e hora de inicio": data_hora_inicio,
-        "data e hora do fim": data_hora_fim,
+        "data_e_hora_de_inicio": data_hora_inicio,
+        "data_e_hora_do_fim": data_hora_fim,
         "ativa": True
     }
 
@@ -31,12 +31,10 @@ def criar_reserva(codigo, usuario, sala, data_hora_inicio, data_hora_fim) -> Non
 
     reservas = obter_reservas()
 
-    reserva_dic = reserva_dic(codigo, usuario, sala, data_hora_inicio, data_hora_fim)
-
-    chaves_dicionario = [key for key in reservas]
+    reserva_dic = reserva(codigo, usuario, sala, data_hora_inicio, data_hora_fim)
 
     # Verifica se uma reserva com esse código já foi criada
-    codigo_ja_existe = reserva_dic['codigo'] in chaves_dicionario
+    codigo_ja_existe = reserva_dic['codigo'] in [key for key in reservas]
     
     if codigo_ja_existe:
         raise ValueError("Uma reserva com esse código já existe!")
@@ -58,3 +56,28 @@ def atualizar_reserva(id: int, nova_reserva: dict) -> None:
 
 def deletar_reserva(id: int) -> None:
     Dicionario_Csv.excluir_linha_arquivo_por_padrao(f'id:{id}', ARQUIVO_LISTA_RESERVAS)
+
+
+def checar_codigo_em_reservas(codigo: str) -> bool:
+    """Checa se um código existe na lista de reservas
+
+    Args:
+        codigo (str): O código, em formato de string. Se o valor não for passado como uma string não funciona
+
+    Returns:
+        bool: True se o código existe, False do contrário
+    """
+
+    reservas = obter_reservas()
+
+    print(reservas)
+
+    for reserva in reservas:
+        if codigo == reserva['codigo']:
+            return True
+
+    return False
+
+# criar_reserva(90, "Latorre", "D4", "08:30", "10:00")
+
+checar_codigo_em_reservas(90)
