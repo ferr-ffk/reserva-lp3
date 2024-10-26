034e02b3054d190e68045e700cebfb076c54e10b
Fernando Freitas <freitax.li@gmail.com>
2024-10-26 15:03:36 -0300

    adicionar pydoc e lógica para encapsular formato de data

--- reserva_app/utils/datetime_utils.py
+++ reserva_app/utils/datetime_utils.py
@@ -1,8 +1,20 @@
 import datetime
 
 
+_FORMATO_DATA = '%Y-%m-%d %H:%M'
+
+
 def converter_str_datetime(s: str) -> datetime:
-    return datetime.datetime.strptime(s, '%Y-%m-%d %H:%M')
+    """Converte uma string no formato padrão do sql em um objeto datetime
+
+    Args:
+        s (str): A string a ser convertida
+
+    Returns:
+        datetime: O objeto datetime com os valores de data e horário vindos da string
+    """
+    
+    return datetime.datetime.strptime(s, _FORMATO_DATA)
 
 
 def converter_datetime_str_mysl(data: datetime.datetime) -> str:
@@ -15,5 +27,32 @@ def converter_datetime_str_mysl(data: datetime.datetime) -> str:
         str: A string, já formatada no padrão de datetime do MySQL
     """
 
-    return data.strftime('%Y-%m-%d %H:%M')
+    return data.strftime(_FORMATO_DATA)
 
+
+def obter_formato_data() -> str:
+    """Obtém o formato que é utilizado para fazer a conversão
+
+    Returns:
+        str: A string do formato
+    """
+    
+    return _FORMATO_DATA
+
+
+def set_formato_data(novo_formato: str) -> None:
+    """Define um novo valor para o formato a ser utilizado para fazer a conversão, seguindo a tabela:
+    <table>
+        <tr><td>%Y</td> <td>(Ano)</td></tr>
+        <tr><td>%m</td> <td>(Mês)</td></tr>
+        <tr><td>%d</td> <td>(Dia)</td></tr>
+        <tr><td>%H</td> <td>(Hora)</td></tr>
+        <tr><td>%M</td><td>(Minuto)</td></tr>
+        <tr><td>%S</td><td> (Segundo)</td></tr>
+    </table>
+
+    Args:
+        novo_formato (str): O novo formato 
+    """
+    
+    _FORMATO_DATA = novo_formato
